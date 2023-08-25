d60390df485602c2698697e60b2a4c23bf9bb612
fahadakbar1 <muhammadfahadakbar1@gmail.com>
2023-08-25 12:47:31 +0500

    Fixes all issues

--- src/redux/reducers/selectedTasksReducer.tsx
+++ src/redux/reducers/selectedTasksReducer.tsx
@@ -1,13 +1,31 @@
 import { AnyAction } from "redux";
 
-const initialState: number[] = []; // Array to store selected task IDs
+export interface Task {
+  id: number;
+  title: string;
+  minBudget: number;
+  maxBudget: number;
+  avgBudget: number;
+  image: string;
+}
+
+export interface SelectedTask {
+  task: Task;
+  categoryId: number; // Associate tasks with category
+}
+
+const initialState: SelectedTask[] = []; // Array to store selected task objects
 
 const selectedTasksReducer = (state = initialState, action: AnyAction) => {
   switch (action.type) {
     case "ADD_TASK":
-      return [...state, action.payload];
+      const { task, categoryId } = action.payload;
+      return [...state, { task, categoryId }];
     case "REMOVE_TASK":
-      return state.filter((id) => id !== action.payload);
+      const taskIdToRemove = action.payload;
+      return state.filter(
+        (selectedTask) => selectedTask.task.id !== taskIdToRemove
+      );
     default:
       return state;
   }
